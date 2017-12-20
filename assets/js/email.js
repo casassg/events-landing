222cca41c6c3024e4d66c59deb4d05667798a513
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-12-19 22:00:21 -0700

    Remove log statments

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,10 +1,6 @@
 $("#email-form").submit(function(e){
     $.post("http://hackcu-sendgrid-capture.herokuapp.com", $("#email-form").serialize(), function(data) {
-    	console.log(data);
     	var j_data = JSON.parse(data);
-    	console.log(j_data["new_count"]);
-    	console.log(j_data["errors"]);
-
     	if(!j_data["errors"].length){
 			$('#email-input').val('');
 			$('#email-success').fadeIn().delay(3500).fadeOut();
